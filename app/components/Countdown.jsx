2ada00d3b269714376c2b86b4daf38d7b637d76a
Ivan <Ivan@MacBook-Pro-de-Ivan.local>
2016-05-19 16:52:56 -0300

    Create Clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,9 +1,14 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function(){
     return (
-      <h1>CountDown.jsx</h1>
+      <div className="row">
+        <div className="small-centered middle-6 large-4">
+          <Clock totalSeconds={129}/>
+        </div>
+      </div>
     );
   }
 });
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,9 +1,14 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Timer = React.createClass({
   render: function(){
     return (
-      <h1>Timer.jsx</h1>
+      <div className="row">
+        <div className="small-centered middle-6 large-4">
+          <Clock totalSeconds={0}/>
+        </div>
+      </div>
     );
   }
 });
